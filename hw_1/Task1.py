77c0da501aaa3981f47be5ee29b1a17f4e91c250
Vasyl Zakharuk <vzakharuk@gmail.com>
2018-10-09 22:39:52 +0300

    First commit

---
+++ hw_1/Task1.py
@@ -0,0 +1,13 @@
+# Vasyl Zakharuk
+# Python
+
+name=input("What is your name?  ")
+print("Hello, ", name)
+
+age=input("How old are You?  ")
+print("Your age is ", age)
+
+place=input("Where do you live?  ")
+print("You live in  ", place)
+
+
---
+++ hw_1/Task2.py
@@ -0,0 +1,9 @@
+4
+# Varialable
+
+a=int(input("Вкажіть число a:  "))
+b=int(input("Вкажіть число b:  "))
+c=a+b
+print(a,"+",b,"=",c)
+c=a-b
+print(a,"-",b,"=",c)
