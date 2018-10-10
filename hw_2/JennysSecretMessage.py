3bb33f8bf9b6eef4673499cc94453426c0fcf01c
V Zakharuk <43881405+VZakharuk@users.noreply.github.com>
2018-10-10 23:19:51 +0300

    Add files via upload

---
+++ hw_2/JennysSecretMessage.py
@@ -0,0 +1,12 @@
+'''
+Vasyl Zakharuk
+Python Core 355
+Codewars Kata: Jenny's Secret Message
+'''
+name=input("Write a name:  ")
+def greet(name):
+    if name == "Johnny":
+        return "Hello, my love!"
+    else:
+        return "Hello, {name}!".format(name=name)
+print(greet(name))
---
+++ hw_2/reverse.py
@@ -0,0 +1,13 @@
+'''
+Vasyl Zakharuk
+Python Core Lv-355
+Codewars Kata: Reversing Words in a String
+'''
+st = input("Введіть фразу: ")
+def reverse(st):
+    line = st.split()
+    line.reverse()
+    st = ' '.join(line)
+    return st
+print(reverse(st))
+
