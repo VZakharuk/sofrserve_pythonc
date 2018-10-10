86b13836d9504a3d2dfe6a7e8e61e52fe54b209c
V Zakharuk <43881405+VZakharuk@users.noreply.github.com>
2018-10-10 22:33:28 +0300

    Update Task2.py

--- hw_1/Task2.py
+++ hw_1/Task2.py
@@ -1,6 +1,8 @@
-# Vasyl Zakharuk
-# Python Core 355
-
+'''
+Vasyl Zakharuk
+Python Core 355
+addition, subtraction, multiplication, division
+'''
 a=int(input("Вкажіть число a:  "))
 b=int(input("Вкажіть число b:  "))
 c=a+b
