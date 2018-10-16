23c808a05f03aab8f9684b377021756bb9f3815a
Vasyl Zakharuk <vzakharuk@gmail.com>
2018-10-16 10:34:36 +0300

    add new path

---
+++ hw_3/will you make it.py
@@ -0,0 +1,12 @@
+'''
+Vasyl Zakharuk
+Python Core 355
+Codewars Kata: Will you make it?
+''' 
+def zero_fuel(distance_to_pump, mpg, fuel_left):
+    if fuel_left >= distance_to_pump / mpg:
+        print("We got to the pump")
+    else:
+        print("We pushed the car to the pump(((")
+print(zero_fuel(50,25,2))
+
