080ef258e8564de3faa0737621e234dfac3692bc
Vasyl Zakharuk <vzakharuk@gmail.com>
2018-10-18 00:03:28 +0300

    add new pass

---
+++ hw_3/Are_You_playing_Banjo_v2.py
@@ -0,0 +1,10 @@
+'''
+Vasyl Zakharuk
+Python Core 355
+Codewars Kata: Are You playing Banjo?
+'''
+def areYouPlayingBanjo(name):
+    return ("{0} plays banjo" if name[0].lower() == 'r'\
+            else "{0} does not play banjo").format(name)
+name = input("Are You playing Banjo ")
+print(areYouPlayingBanjo(name))
--- hw_3/will_you_make_it.py
+++ hw_3/will_you_make_it.py
@@ -2,12 +2,12 @@
 Vasyl Zakharuk
 Python Core 355
 Codewars Kata: Will you make it?
-''' 
+'''
 def zero_fuel(distance_to_pump, mpg, fuel_left):
-    if fuel_left >= distance_to_pump / mpg:
-        print("We got to the pump")
-        return True
-    else:
-        print("We pushed the car to the pump(((")
-        return False
-print(zero_fuel(50,25,2))
+    return ("We got to the pump" if fuel_left >= distance_to_pump / mpg\
+            else "A few miles we were pushing the car to the pump(((").format(distance_to_pump,\
+                    mpg, fuel_left)
+distance_to_pump = int(input("Please input distanse to pump: "))
+mpg = int(input("Please input miles per gallon: "))
+fuel_left = int(input("Please input fuel left: "))
+print(zero_fuel(distance_to_pump, mpg, fuel_left))
