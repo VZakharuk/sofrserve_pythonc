594e37d73e9fa110fca2a9871ebef0ac71696ece
Vasyl Zakharuk <vzakharuk@gmail.com>
2018-10-18 16:26:21 +0300

    add new pass

---
+++ hw_3/conver_boolean_values_to_string_'Yes'_or_'No'.py
@@ -0,0 +1,7 @@
+'''
+Vasyl Zakharuk
+Python Core 355
+Codewars Kata: Convert boolean values to string 'Yes' or 'No'
+'''
+def bool_to_word(boolean):
+   return "Yes" if boolean else "No".format(boolean)
