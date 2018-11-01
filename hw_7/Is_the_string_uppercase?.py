2e1f5148c804fb2c7314f16d0741c9c386389816
Vasyl Zakharuk <vzakharuk@gmail.com>
2018-11-01 17:55:38 +0200

    add new pass

---
+++ hw_7/Is_the_string_uppercase?.py
@@ -0,0 +1,13 @@
+"""
+Vasyl Zakharuk
+Python Core 355
+Codewars Kata: Is the string uppercase
+"""
+
+
+def is_uppercase(inp):
+    caps = inp.upper()
+    if caps == inp:
+        return True
+    else :
+        return False
