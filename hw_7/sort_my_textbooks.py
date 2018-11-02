db942f80d0a02952b440901a21d22a0f9e94eee8
Vasyl Zakharuk <vzakharuk@gmail.com>
2018-11-02 21:21:46 +0200

    add new pass

--- hw_7/sort_my_textbooks.py
+++ hw_7/sort_my_textbooks.py
@@ -7,8 +7,15 @@ Codewars Kata. Sort my textbooks
 textbooks = input("Entry the list of books: ")
 
 def sorter(textbooks):
-    return(sorted(textbooks, key=lambda x: x[0].upper()))
+    return sorted(textbooks, key=lambda x: x[0].upper())
 
-print(sorter([textbooks])) # parametr textbooks must to have [] like a list!!!
+print(sorter([textbooks])) # not workin properly
 
 
+#def sorter(textbooks):
+#    textbooks.sort(key=str.upper) 
+#    return textbooks
+
+#print(sorter(['Algebra', 'history', 'Geometry', 'english']))
+
+#print(sorter(textbooks))) # parametr textbooks must to have [] like a list!!!
