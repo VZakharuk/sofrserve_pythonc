1d2773f141b3104e086a45757340cb3e947eeba2
Vasyl Zakharuk <vzakharuk@gmail.com>
2018-11-09 13:41:00 +0200

    add new pass

--- ClassWork/cw9/cw_9_4_Days_of_week.py
+++ ClassWork/cw9/cw_9_4_Days_of_week.py
@@ -12,16 +12,12 @@ week_days = {1: 'Monday',
              5: 'Friday',
              6: 'Saturday',
              7: 'Sunday'}
-# This function has not sens if I can't print: The day of week is...
-#def day_num(i):
-#    if i >= 8 or i <= 0:
-#        raise ValueError('You did not enter the right number. The week has seven days')
+
 while True:
     try:
         i = int(input('Enter the day of week: '))
-#        day_num(i)
     except ValueError:
         print('You did not enter the number!')
     else:
         print(week_days.get(i, 'You did not enter the right number. The week has seven days')) # .get is need that never return KeyError
-#        print("The day of week is {}".format(week_days[i]))
+
