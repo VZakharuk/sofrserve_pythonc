f68d70e937ea9903f6f58bcdfed7e85442b04790
Vasyl Zakharuk <vzakharuk@gmail.com>
2018-11-15 13:41:49 +0200

    new pass

--- hw_11/hw_11_game_random_number.py
+++ hw_11/hw_11_game_random_number.py
@@ -5,13 +5,19 @@ Home work 11. Game: random number.
 """
 
 
-import random
+from random import randint
+
+
 robot_chappy = random.randint(1, 100)  # because I like him
 human_user = int(input("Please input your number: "))  # this's me or anybody else
+
+
 while human_user != robot_chappy:
     if human_user < robot_chappy:
         print("The number is too small!")
     elif human_user > robot_chappy:
         print("The number is too large!")
     human_user = int(input("Please input your number: "))
+
+
 print("You have selected the correct number!")
