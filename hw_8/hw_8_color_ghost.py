94f892f8481fe384c1cdceb0741d1a36e818e10f
Vasyl Zakharuk <vzakharuk@gmail.com>
2018-11-15 13:02:12 +0200

    new pass

--- hw_8/hw_8_color_ghost.py
+++ hw_8/hw_8_color_ghost.py
@@ -8,13 +8,26 @@ Codewars Kata. Color Ghost.
 # Ghost objects are instantiated without any arguments.
 # Ghost objects are given a random color attribute of white" or "yellow" or "purple" or "red" when instantiated
 
-import random
+# better variant
+from random import choice
 
 
 class Ghost(object):
-    def __init__(self, color):
-        self.color = random.choice(["white", "yellow", "purple", "red"])
+    def __init__(self):
+        self.color = choice(["white", "yellow", "purple", "red"])
 
 
-color_ghost = Ghost(object)  # on codewars-site must be empty Ghost()
+color_ghost = Ghost()
 print(color_ghost.color)
+
+# # worth variant
+# import random
+#
+#
+# class Ghost(object):
+#     def __init__(self):
+#         self.color = random.choice(["white", "yellow", "purple", "red"])
+#
+#
+# color_ghost = Ghost()
+# print(color_ghost.color)
